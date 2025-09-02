6ce085f77fb2412caf0aaaa7b67ca8c2f5baf9ee
Santanu Paul <matainja0141@gmail.com>
2025-09-02 13:51:21 +0530

    PRICING UPDATE 2

--- application/controllers/NewProduct.php
+++ application/controllers/NewProduct.php
@@ -461,29 +461,36 @@ class NewProduct extends MW_Controller
                         // Check if sku already exists in product_pricings
                         $pricing_exists = $this->db->get_where('product_pricings', [
                             'product_id' => $product->id,
-                            'sku'        => $sku_code
+                            // 'sku'        => $sku_code
                         ])->row();
 
                         $vendor_id = $this->input->post('vendor_id');
-                        // if (!$pricing_exists) {
-                        //     $this->db->insert('product_pricings', [
-                        //         'product_id'        => $product->id,
-                        //         'sku'               => $sku_code,
-                        //         'vendor_product_id' => $product->mpn,   // mpn from products table
-                        //         'matix_id'          => $product->matix_id,
-                        //         'minimum_threshold' => 0,
-                        //         'vendor_id'         => $vendor_id,
-                        //         'price'             => null,
-                        //         'retail_price'      => null,
-                        //         'active'            => 1,
-                        //         'quantity'          => null,
-                        //         'exclude_from_marketplace' => 0,
-                        //         'exclude_from_whitelabels_1' => 0,
-                        //         'exclude_from_whitelabels_2' => 0,
-                        //         'created_at'        => date('Y-m-d H:i:s'),
-                        //         'updated_at'        => date('Y-m-d H:i:s'),
-                        //     ]);
-                        // }
+                        if (!$pricing_exists) {
+                            $this->db->insert('product_pricings', [
+                                'product_id'        => $product->id,
+                                'sku'               => $sku_code,
+                                'vendor_product_id' => $product->mpn,   // mpn from products table
+                                'matix_id'          => $product->matix_id,
+                                'minimum_threshold' => 0,
+                                'vendor_id'         => $vendor_id,
+                                'price'             => null,
+                                'retail_price'      => null,
+                                'active'            => 1,
+                                'quantity'          => null,
+                                'exclude_from_marketplace' => 0,
+                                'exclude_from_whitelabels_1' => 0,
+                                'exclude_from_whitelabels_2' => 0,
+                                'created_at'        => date('Y-m-d H:i:s'),
+                                'updated_at'        => date('Y-m-d H:i:s'),
+                            ]);
+                        }
+                        else{
+                            $this->db->update('product_pricings', [
+                                'sku'               => $sku_code,
+                                'created_at'        => date('Y-m-d H:i:s'),
+                                'updated_at'        => date('Y-m-d H:i:s'),
+                            ]);
+                        }
                     }
                 }
             }
